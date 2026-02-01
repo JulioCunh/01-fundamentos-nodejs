fe93a4c69c9055cdfddd08f4e71597a477b4447a
Expert 🚀 <fcezarf@gmail.com>
2026-02-01 19:18:02 -0300

    Route e Query parameters

--- src/routes.js
+++ src/routes.js
@@ -30,4 +30,15 @@ export const routes = [
       return res.writeHead(201).end();
     },
   },
+  {
+    method: "DELETE",
+    path: "/users/:id",
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      database.delete("users", id);
+
+      return res.writeHead(204).end();
+    },
+  },
 ];
